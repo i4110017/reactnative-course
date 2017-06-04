247a72b5d4208f5a920531889c9c61313ba914b7
Mintra Ruensuk <kikist@gmail.com>
2017-06-04 11:25:53 +0700

    Add axios

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -2,12 +2,16 @@
 
 // 1. Import a library to help create a compoment
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // 2. Create a Component
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // 3. Render it to the device
