eb24829be7df837f23c5116448e2558ef69010b5
Mintra Ruensuk <kikist@gmail.com>
2017-06-06 12:35:50 +0700

    Import header to the app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <View>
+        <Header headerText="Authenthication" />
         <Text>An App!</Text>
       </View>
     );
   }
-};
+}
 export default App;
